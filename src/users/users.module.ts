40e8f80b8e1ec1dbb663a80eeeb506e6a12f4f50
arsiihere <arsiiahmi@gmail.com>
2022-08-18 01:05:48 +0500

    creating User entity and list it to its parent module and the roort module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
+import { User } from './user.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
